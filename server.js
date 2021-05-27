e94a4b14ca54e175884d2d577d17e13b8b667452
Rakesh <iamrakeshkumar@pm.me>
2021-05-27 10:21:18 +0530

    changing env

--- server.js
+++ server.js
@@ -33,22 +33,13 @@ app.use(express.json());
 app.use(cookieParser());
 
 // cors
-// if (process.env.NODE_ENV === "production") {
-//   app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
 // if (process.env.NODE_ENV === "production") {
 //   app.use(cors());
 // }
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
+
 // app.use(cors());
 
 app.get("/", (req, res) => {
