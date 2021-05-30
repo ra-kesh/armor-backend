db90caa7aa0e5780ec422e209d3b205c3fa00e04
Rakesh <iamrakeshkumar@pm.me>
2021-05-30 08:36:57 +0530

    chore:changing products list

--- data/productData.js
+++ data/productData.js
@@ -3,18 +3,30 @@ import faker from "faker";
 faker.seed(123);
 
 const jacketArray = [
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Tornado_Pro_3_Jacket_Orange_1_376x.jpg?v=1611827730",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Tornado_Pro_3_Jacket_Blue_1_376x.jpg?v=1611827799",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Tornado_Pro_3_Jacket_Red_1_376x.jpg?v=1611828010",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Tornado_Pro_3_Jacket_Black_1_376x.jpg?v=1611828010",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Storm_Off_White-01_376x.jpg?v=1610443950",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_AirGT_3_Jacket_Black-01_376x.jpg?v=1607064220",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_AirGT_3_Jacket_Dark_Grey-Red-01_376x.jpg?v=1607064220",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_AirGT_3_Jacket_Grey-Hi-viz_Green-01_376x.jpg?v=1607064063",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/RynoxUrbanJacket_EarthBrown-01_376x.jpg?v=1606896792",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/RynoxUrbanJacket_StoneGrey-01_376x.jpg?v=1606896792",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/RynoxUrbanJacket_BattleGreen-01_376x.jpg?v=1606884954",
-  "https://cdn.shopify.com/s/files/1/1947/9551/products/Rynox_Storm_Grey-01_376x.jpg?v=1610447203",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/SCIMITARMetroJacket-blackAndFlGrn-1.1_1800x1800.jpg?v=1591594448",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/SCIMITAR-Razor-_Black-And-Fl-Green_1.1_1800x1800.jpg?v=1610527959",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Viper-green-1_1800x1800.jpg?v=1573821643",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Macna-Event-Riding-Jacket-_White_1_1800x1800.jpg?v=1572872104",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Rewind--white-1_1800x1800.jpg?v=1572873402",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/AEROCON-1_1800x1800.jpg?v=1607343053",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Macna-Equator-Riding-Jacket-_Grey_1_1800x1800.jpg?v=1572871637",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Scimitar-Razor-Jacket_write-and-Fl-Green-And-Blue_-1.1_28ec657b-6f1c-4fcd-816e-244d6e745d5c_1800x1800.jpg?v=1610528342",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/ORCANO-1_1800x1800.jpg?v=1607344009",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/ORCANO-Red-1_1800x1800.jpg?v=1614164035",
+  "https://cdn.shopify.com/s/files/1/1045/6728/products/Hurracage-2_1800x1800.jpg?v=1572871083",
+];
+
+const jacketNameArray = [
+  "Rewind--white-1",
+  "SCIMITAR MetroJacket",
+  "SCIMITAR-Razor",
+  "Viper-green-1",
+  "Macna-Event-Riding-Jacket",
+  "AEROCON-1",
+  "Macna-Equator",
+  "Scimitar-Razor",
+  "ORCANO-1",
+  "Hurracage-2",
 ];
 
 const helmetArray = [
@@ -47,7 +59,7 @@ const shoeArray = [
 
 export const brandNameArray = ["RYNOX", "MUNRO", "SCIMITAR", "MACNA", "ASPIDA"];
 
-const nameArray = ["ADVENTURE-BLACK-1", "Ryo-Onex", "Voyage-1", "Braaap-1"];
+const shoeNameArray = ["ADVENTURE-BLACK-1", "Ryo-Onex", "Voyage-1", "Braaap-1"];
 
 const descriptionArray = [
   "dsiorjvioj vihrevhreuvggvy gyvgdiuvuidvhiudfhvi udfhuvugygvyergvyerugerw8uvg87vg",
@@ -67,9 +79,9 @@ const countArray = [2, 20, 50, 6, 10, 15];
 
 const categoryArray = ["jackets", "helmets", "shoes", "gloves"];
 
-export const products = [...Array(4)].map((item) => ({
-  name: faker.random.arrayElement(nameArray),
-  image: faker.random.arrayElement(shoeArray),
+export const products = [...Array(10)].map((item) => ({
+  name: faker.random.arrayElement(jacketNameArray),
+  image: faker.random.arrayElement(jacketArray),
   brand: faker.random.arrayElement(brandNameArray),
   description: faker.random.arrayElement(descriptionArray),
   price: faker.random.arrayElement(priceArray),
@@ -77,6 +89,6 @@ export const products = [...Array(4)].map((item) => ({
   inStock: faker.datatype.boolean(),
   rating: faker.random.arrayElement(ratingArray),
   quantity: 1,
-  category: "shoes",
+  category: "jackets",
   discount: faker.random.arrayElement(discountArray),
 }));
