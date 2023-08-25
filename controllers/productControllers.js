f286a72f8b704a33db5f072ed106adf107f33d07
ra-kesh <iamrakeshkumar@pm.me>
2023-08-25 13:37:09 +0530

    feat:added features for pagination and select in products

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -2,7 +2,32 @@ import { Product } from "../models/productModel.js";
 import expressAsyncHandler from "express-async-handler";
 
 const getAllProducts = expressAsyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const { keyword, limit, skip, select, sort } = req.query;
+
+  const query = {};
+
+  if (keyword) {
+    query.name = { $regex: keyword, $options: "i" };
+  }
+
+  let selectFields = "";
+  if (select) {
+    selectFields = select.split(",").join(" ");
+  }
+
+  const options = {
+    limit: parseInt(limit) || 10,
+    skip: parseInt(skip) || 0,
+    sort: sort || null,
+  };
+
+  const products = await Product.find(query)
+    .select(selectFields)
+    .skip(options.skip)
+    .limit(options.limit)
+    .sort(options.sort)
+    .exec();
+
   res.status(200).json({ success: true, data: products });
 });
 
